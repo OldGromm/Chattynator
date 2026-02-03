6a8de78417ed9d3c46519b5a53cb412b61a408af
OldGromm <oldgromm@protonmail.com>
2026-02-03 15:45:38 +0100

    Update Config.lua

--- Core/Config.lua
+++ Core/Config.lua
@@ -78,7 +78,8 @@ local settings = {
   },
 
   EDIT_BOX_POSITION = {key = "edit_box_position", default = "bottom"},
-
+  KEEP_EDIT_BOX_VISIBLE = {key = "keep_edit_box_visible", default = false},
+  
   SKINS = {key = "skins", default = {}},
   DISABLED_SKINS = {key = "disabled_skins", default = {}},
   CURRENT_SKIN = {key = "current_skin", default = "dark"},
